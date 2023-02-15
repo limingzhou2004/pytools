3e6d4492a61ce7a36a2e770f3403f8420902b4e4
liming zhou <liming.zhou2004@gmail.com>
2023-02-15 00:17:23 -0500

    50

--- airflow/download_hrrr_obs.py
+++ airflow/download_hrrr_obs.py
@@ -23,7 +23,7 @@ args={
 with DAG(
     "hrrr_obs", start_date=pu.datetime(2023, 1, 1, tz="UTC"),
     dagrun_timeout=args['time_out'],
-    schedule="15 * * * *", catchup=False, tags=['hrrr','liming']
+    schedule="50 * * * *", catchup=False, tags=['hrrr','liming']
 ) as dag:
     # airflow variables set [-h] [-j] [-v] key VALUE    
     py_path = Variable.get('py_path',default_var=None)
