653dc13ce2a6e64baf904be1926ff4fbe0611fe8
liming zhou <liming.zhou2004@gmail.com>
2024-07-22 11:12:32 -0400

    updated summary of grib data to use version

--- test/test_data_prep/test_grib_util_org.py
+++ test/test_data_prep/test_grib_util_org.py
@@ -9,8 +9,8 @@ def test_load_files():
     load_files(-1)
     assert 1==1
 
-@pytest.mark.skip(reason='grib files')
+#@pytest.mark.skip(reason='grib files')
 def test_make_stats():
-    make_stats()
+    make_stats(i=1)
 
     assert 1==1
