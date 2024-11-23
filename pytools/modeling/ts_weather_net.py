77ee51db7f49b8004e8c00e2fc64b0c39be5e23c
liming zhou <liming.zhou2004@gmail.com>
2024-11-23 14:14:33 -0500

    add shape input

--- pytools/modeling/ts_weather_net.py
+++ pytools/modeling/ts_weather_net.py
@@ -6,8 +6,33 @@ from pytools.modeling.weather_net import WeatherNet
 
 class WeaCov(nn.Module):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, input_shape, layer_paras):
+        # input_shape, (batch, x, y, channel)
+        super().__init__()
+        self.weather_conv1_layer = nn.Sequential(
+            nn.Conv2d(
+                in_channels=weather_para.channel,
+                out_channels=layer_paras["w_out1_channel"],
+                kernel_size=layer_paras["w_kernel1"],
+                stride=layer_paras["w_stride1"],
+                padding=layer_paras["w_padding_1"],
+            ),
+            nn.LayerNorm(num_features=layer_paras["w_out1_channel"]),
+            nn.ReLU(),
+            # nn.Dropout2d(p=model_settings.dropout),
+        )
+        self.weather_conv2_layer = nn.Sequential(
+            nn.Conv2d(
+                in_channels=layer_paras["w_out1_channel"],
+                out_channels=layer_paras["w_out2_channel"],
+                kernel_size=layer_paras["w_kernel2"],
+                stride=layer_paras["w_stride2"],
+                padding=layer_paras["w_padding_2"],
+            ),
+            nn.LayerNorm(num_features=layer_paras["w_out2_channel"]),
+            nn.ReLU(),
+            # nn.Dropout2d(p=model_settings.dropout),
+        )
 
     def forward(self, wea_arr):
 
--- test/test_modeling/test_ts_weathernet.py
+++ test/test_modeling/test_ts_weathernet.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from pytools.config import Config
 from pytools.modeling.dataset import WeatherDataSet, check_fix_missings, read_weather_data_from_config
+from pytools.modeling.ts_weather_net import WeaCov
 from pytools.modeling.weather_net import WeatherLayer
 
 
@@ -18,7 +19,7 @@ def test_construct_weathernet(config:Config):
     wds = WeatherDataSet(flag='cv',tabular_data=load_arr, wea_arr=wea_arr, timestamp=t, config=config, sce_ind=0)
     wds1 = WeatherDataSet(flag='final_train',tabular_data=load_arr, wea_arr=wea_arr, timestamp=t, config=config, sce_ind=0)
 
-    w=WeatherLayer()
+    w=WeaCov(config.model_pdt.cov_layer)
 
 
     assert 1==1
