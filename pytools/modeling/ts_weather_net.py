066ff16f6d7864a4c208b74d591f232cbb99b773
liming zhou <liming.zhou2004@gmail.com>
2024-11-28 22:56:20 -0500

    add ts weather

--- pytools/modeling/ts_weather_net.py
+++ pytools/modeling/ts_weather_net.py
@@ -80,7 +80,30 @@ class WeaCov(nn.Module):
 
 class TSWeatherNet(WeatherNet):
 
-    def __init__(self, ):
+    def __init__(self, wea_arr_shape, wea_layer_paras, seq_dim=1):
+        # wea_arr_shape, N, Seq, x, y, channel/para
         super().super().__init__()
+        self._wea_arr_shape = wea_arr_shape.copy()
+        self._seq_dim = seq_dim
+        del wea_arr_shape[seq_dim]
+        self.wea_net = WeaCov(input_shape=wea_arr_shape, layer_paras=wea_layer_paras)
+
 
         return
+    
+    def forward(self, seq_wea_arr, ext_wea_arr):
+        seq_wea_arr = seq_wea_arr.detach().clone()
+        channel_num = self.wea_net.output_shape[1]
+        wea_len = ext_wea_arr.shape[self._seq_dim]
+        seq_length = seq_wea_arr.shape[self._seq_dim]
+        seq_pred = torch.zeros([seq_length,channel_num])
+        wea_pred = torch.zeros(ext_wea_arr.shape[self._seq_dim], channel_num)
+        for i in range(seq_length):
+            seq_pred[:,i,:] = self.wea_net.forward(seq_wea_arr[:,i,...])
+        for i in range(wea_len):
+            wea_pred[:,i,:] = self.wea_net.forward(ext_wea_arr[:,i,...])
+
+
+        wea_embed = nn.Linear()
+
+        return seq_pred, wea_pred
