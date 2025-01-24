a4cd42bafb9a95fb206507fbaaa6a2bf4764c20e
liming zhou <liming.zhou2004@gmail.com>
2025-01-24 08:11:21 -0500

    clean temp files

--- test.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/miniconda3
-curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
